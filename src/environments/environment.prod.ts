d9d730aab453318b37038e5c06ec7405b9f137ac
javier hermosillo <hermosillo.javier@outlook.com>
2018-10-22 22:33:57 -0500

    change port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
 
   production: true,
-  apiUrl: 'https://grinnav.herokuapp.com/api'
+  // apiUrl: 'https://grinnav.herokuapp.com/api'
+  apiUrl: 'http://localhost:3000/api'
+
 };
